cb72ed474cdbf414bffa90b659dab2520437ca81
KonlawatPetch <pach.wongsrithong@Gmail.com>
2023-09-21 13:15:30 +0700

    add css file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// base: "/portfolio/",
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/",
   plugins: [react()],
 })
