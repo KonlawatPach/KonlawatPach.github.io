4927d85333ea3c55b4c83d538e057e4a0780b0c6
KonlawatPach <pach.wongsrithong@Gmail.com>
2023-09-22 18:36:08 +0700

    update animation start & title

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,33 @@ function App() {
   const [theme, setTheme] = useState('light');
   const [language, setLanguage] = useState('TH');
   useEffect(() => {
-    // document.addEventListener('keydown', (event) => {
-    //   if (event.key === 'Tab') {
-    //     console.log('User pressed the Tab key');
-    //   }
-    // });
-    console.log(data);
+    // Create and configure the IntersectionObserver
+    const observer = new IntersectionObserver((entries) => {
+      // document.addEventListener('keydown', (event) => {
+      //   if (event.key === 'Tab') {
+      //     console.log('User pressed the Tab key');
+      //   }
+      // });
+      entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    });
+
+    // Select and observe hidden elements
+    const hiddenElementLeft = Array.from(document.querySelectorAll('.hiddenl'));
+    const hiddenElementRight = Array.from(document.querySelectorAll('.hiddenr'));
+    const hiddenElement = hiddenElementLeft.concat(hiddenElementRight);
+    hiddenElement.forEach((el) => observer.observe(el));
+
+    // Clean up the observer when the component unmounts
+    return () => {
+      hiddenElement.forEach((el) => observer.unobserve(el));
+    };
   }, []);
 
   const switchTheme = () => {
@@ -33,6 +54,7 @@ function App() {
         <div className='body'>
 
         </div>
+        {/* <div className="hiddenl">aaaaaaaaaaaaaaaaaaaaaaaa</div> */}
       </div>
     </>
   )
--- src/components/title.jsx
+++ src/components/title.jsx
@@ -5,37 +5,51 @@ function Title(props) {
 
     return (
         <div className='title'>
-            <div className='bluebox' style={{width:'70vw', height: '91px'}}></div>
-            <div className='absolute' style={{height: '70px'}}>  
-                <div className='blueboxlight' style={{width:'260px', height: '100px'}}>
+            <div className='bluebox hiddenr' style={{width:'70vw', height: '70px'}}></div>
+            <div className='absolute' style={{height: '40px'}}>  
+                <div className='blueboxlight hiddenr' style={{width:'260px', height: '70px'}}>
                     <img onClick={props.switchTheme} src={props.theme=='light'?"title/sun.svg":"title/moon.svg"} alt="sun"/>
                     <h3 onClick={props.switchLanguage}>{props.language}</h3>
                 </div>
             </div>
             
             <div className='namebox'>
-                <h1>{props.language=='TH'?props.data.fNameTH:props.data.fNameEN}</h1>
-                <h1>{props.language=='TH'?props.data.lNameTH:props.data.lNameEN}</h1>
-                <p>{props.language=='TH'?props.data.positionTH:props.data.positionEN}</p>
+                <h1 className='hiddenr'>{props.language=='TH'?props.data.fNameTH:props.data.fNameEN}</h1>
+                <h1 className='hiddenr'>{props.language=='TH'?props.data.lNameTH:props.data.lNameEN}</h1>
+                <p className='hiddenr'>{props.language=='TH'?props.data.positionTH:props.data.positionEN}</p>
             </div>
 
-            <div className='absolute' style={{height: '100px'}}>
-                <div className='titlebluebottom1 boxbottom' style={{width:'26vw', height: '120px', top: '20px', right: '6vw'}}></div>               
-            </div>
-            <div className='absolute' style={{height: '0'}}>
-                <div className='titlebluebottom2 boxbottom' style={{width:'60vw', height: '200px', bottom: '40px', right: '30vw'}}></div>               
-            </div>
-            <div className='absolute' style={{height: '0'}}>
-                <div className='titlebluebottom3 boxbottom' style={{width:'20vw', height: '150px', bottom: '60px', right: '80vw'}}></div>               
-            </div>
-            <div className='absolute' style={{height: '0'}}>
-                <div className='titlebluebottom4 boxbottom' style={{width:'50vw', height: '150px', top: '0px', right: '0vw'}}></div>               
-            </div>
-            <div className='absolute' style={{height: '0'}}>
-                <div className='titlegreenbottom1 boxbottom' style={{width:'66vw', height: '150px', top: '50px', right: '30vw'}}></div>               
-            </div>
-            <div className='absolute' style={{height: '0'}}>
-                <div className='titlegreenbottom2 boxbottom' style={{width:'26vw', height: '180px', top: '40px', right: '78vw'}}></div>               
+            <div className="bottomBox">
+                <div className='absolute' style={{height: '0'}}>
+                    <div className='titlebluebottom1 boxbottom' style={{width:'28vw', height: '8vw', bottom: '8vw', left: '60vw'}}></div>               
+                </div>
+                <div className='absolute' style={{height: '0'}}>
+                    <div className='titlebluebottom2 boxbottom' style={{width:'70vw', height: '8vw', bottom: '6vw', left: '0vw'}}></div>               
+                </div>
+                <div className='absolute' style={{height: '0'}}>
+                    <div className='titlebluebottom3 boxbottom' style={{width:'20vw', height: '12vw', bottom: '7vw', left: '0vw'}}></div>               
+                </div>
+                <div className='absolute' style={{height: '0'}}>
+                    <div className='titlebluebottom4 boxbottom' style={{width:'44vw', height: '10vw', bottom: '2vw', left: '56vw'}}></div>               
+                </div>
+                <div className='absolute' style={{height: '0'}}>
+                    <div className='titlegreenbottom1 boxbottom' style={{width:'66vw', height: '10vw', bottom: '0vw', left: '0vw'}}></div>               
+                </div>
+                <div className='absolute' style={{height: '0'}}>
+                    <div className='titlegreenbottom2 boxbottom' style={{width:'32vw', height: '13vw', top: '1vw', left: '0vw'}}></div>               
+                </div>
+                <div className='absolute' style={{height: '0'}}>
+                    <div className='titlegreenbottom3 boxbottom' style={{width: '52vw', height: '9vw', top: '9vw', left: '8vw'}}></div>               
+                </div>
+                <div className='absolute' style={{height: '0'}}>
+                    <div className='titlegreenbottom4 boxbottom' style={{width: '40vw', height: '7vw', top: '5vw', left: '30vw'}}></div>               
+                </div>
+                <div className='absolute' style={{height: '0'}}>
+                    <div className='titlegreenbottom5 boxbottom' style={{width: '48vw', height: '9vw', top: '7vw', left: '52vw'}}></div>               
+                </div>
+                <div className='absolute' style={{height: '0'}}>
+                    <div className='titlegreenbottom6 boxbottom' style={{width: '53vw', height: '7vw', top: '10vw', left: '38vw'}}></div>               
+                </div>
             </div>
         </div>
     )
